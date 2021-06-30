9663a0227be65aea92c89736f2b23357baa3f777
asifahmed <asifahmed.mit@gmail.com>
2021-06-30 17:06:13 +0600

    added navigation drawer

--- app/src/main/java/org/timerrubikscube/DashboardActivity.kt
+++ app/src/main/java/org/timerrubikscube/DashboardActivity.kt
@@ -3,10 +3,16 @@ package org.timerrubikscube
 import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 import com.yashovardhan99.timeit.Stopwatch
 import org.timerrubikscube.nonactivityclass.ScrambleGenerator
 
@@ -18,14 +24,21 @@ class DashboardActivity : AppCompatActivity() {
     lateinit var startTimeBtn: Button
 
 
+    lateinit var navigationView: NavigationView
+    lateinit var toggle: ActionBarDrawerToggle
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var toolbar: Toolbar
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
 
-        tvScramble = findViewById(R.id.dashboard_scramble_tv)
-        nextBtn = findViewById(R.id.dashboard_next)
-        tvTime = findViewById(R.id.dashboard_time)
-        startTimeBtn = findViewById(R.id.dashboard_play)
+        initVariables()
+
+        clickListeners()
+
+
         tvScramble.text = ScrambleGenerator().giveScramble()
 
         var stopwatch: Stopwatch = Stopwatch()
@@ -45,5 +58,42 @@ class DashboardActivity : AppCompatActivity() {
             }
         })
 
+    }
+
+    private fun clickListeners() {
+        navigationView.setNavigationItemSelectedListener(NavigationView.OnNavigationItemSelectedListener { menuitem ->
+            when (menuitem.itemId) {
+                R.id.nav_menu_star -> {
+                    println("star")
+                    drawerLayout.closeDrawer(GravityCompat.START)
+                }
+                R.id.nav_menu_history -> {
+                    println("history")
+                    drawerLayout.closeDrawer(GravityCompat.START)
+                }
+                R.id.nav_menu_settings -> {
+                    println("settings")
+                    drawerLayout.closeDrawer(GravityCompat.START)
+                }
+                else -> {
+                    println("Rip")
+                }
+            }
+            true
+        })
+    }
+
+    private fun initVariables() {
+        tvScramble = findViewById(R.id.dashboard_scramble_tv)
+        nextBtn = findViewById(R.id.dashboard_next)
+        tvTime = findViewById(R.id.dashboard_time)
+        startTimeBtn = findViewById(R.id.dashboard_play)
+        toolbar = findViewById(R.id.dashboard_toolbar)
+        setSupportActionBar(toolbar)
+        navigationView = findViewById(R.id.nav_view_dashboard)
+        drawerLayout = findViewById(R.id.drawer_dashboard)
+        toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
     }
 }
