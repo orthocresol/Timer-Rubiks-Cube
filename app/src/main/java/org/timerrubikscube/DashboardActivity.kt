75f68c283a595b25c5bf50e82e41df4fff35a04f
asifahmed <asifahmed.mit@gmail.com>
2021-06-30 15:57:11 +0600

    added timer

--- app/src/main/java/org/timerrubikscube/DashboardActivity.kt
+++ app/src/main/java/org/timerrubikscube/DashboardActivity.kt
@@ -1,16 +1,22 @@
 package org.timerrubikscube
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
+import com.yashovardhan99.timeit.Stopwatch
 import org.timerrubikscube.nonactivityclass.ScrambleGenerator
 
 class DashboardActivity : AppCompatActivity() {
 
     lateinit var tvScramble: TextView
     lateinit var nextBtn: ImageButton
+    lateinit var tvTime: TextView
+    lateinit var startTimeBtn: Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,11 +24,26 @@ class DashboardActivity : AppCompatActivity() {
 
         tvScramble = findViewById(R.id.dashboard_scramble_tv)
         nextBtn = findViewById(R.id.dashboard_next)
+        tvTime = findViewById(R.id.dashboard_time)
+        startTimeBtn = findViewById(R.id.dashboard_play)
         tvScramble.text = ScrambleGenerator().giveScramble()
+
+        var stopwatch: Stopwatch = Stopwatch()
+        stopwatch.setTextView(tvTime)
 
         nextBtn.setOnClickListener(View.OnClickListener {
             var scramble = ScrambleGenerator().giveScramble()
             tvScramble.text = scramble
         })
+        startTimeBtn.setOnClickListener(View.OnClickListener {
+            if (!stopwatch.isStarted) {
+                stopwatch.start()
+                tvScramble.text = ScrambleGenerator().giveScramble()
+            }
+            else {
+                stopwatch.stop()
+            }
+        })
+
     }
 }
